76839e5c1039fad1b0c68cc442403be4ddef75dc
Joe Dei Rossi <deirossijoseph@gmail.com>
2022-05-17 22:23:44 +0000

    Added functionality for the arrows, clicking them now cycles through the images

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -24,3 +24,44 @@ dotContainer.addEventListener('click', event => {
     }
   }
 });
+
+const nextArrow = document.querySelector('.fa-chevron-right');
+
+nextArrow.addEventListener('click', event => {
+  for (let i = 0; i < dots.length; i++) {
+    if (dots[i].className === 'fa-solid fa-circle') {
+      dots[i].className = 'fa-regular fa-circle';
+      if (dots[i + 1]) {
+        dots[i + 1].className = 'fa-solid fa-circle';
+        img.setAttribute('src', images[i + 1].src);
+        img.setAttribute('alt', images[i + 1].alt);
+        return;
+      } else {
+        dots[0].className = 'fa-solid fa-circle';
+        img.setAttribute('src', images[0].src);
+        img.setAttribute('alt', images[0].alt);
+      }
+    }
+  }
+});
+
+const previousArrow = document.querySelector('.fa-chevron-left');
+
+previousArrow.addEventListener('click', event => {
+  for (let i = 0; i < dots.length; i++) {
+    if (dots[i].className === 'fa-solid fa-circle') {
+      dots[i].className = 'fa-regular fa-circle';
+      if (dots[i - 1]) {
+        dots[i - 1].className = 'fa-solid fa-circle';
+        img.setAttribute('src', images[i - 1].src);
+        img.setAttribute('alt', images[i - 1].alt);
+        return;
+      } else {
+        dots[dots.length - 1].className = 'fa-solid fa-circle';
+        img.setAttribute('src', images[dots.length - 1].src);
+        img.setAttribute('alt', images[dots.length - 1].alt);
+        return;
+      }
+    }
+  }
+});
