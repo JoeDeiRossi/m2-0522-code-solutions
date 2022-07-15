8be98462d5b6f614d196bdaba93a9f24c1a0de83
Joe Dei Rossi <deirossijoseph@gmail.com>
2022-07-15 01:52:19 +0000

    Clients can replace a note (PUT) by id. Project complete

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -91,6 +91,43 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
+// Clients can replace a note (PUT) by id
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (parseInt(id) <= 0 || isNaN(id)) {
+    res.status(400);
+    res.json({ error: 'id must be a positive integer' });
+    return;
+  }
+
+  if (!req.body.content) {
+    res.status(400);
+    res.json({ error: 'must include a content property in the request body' });
+    return;
+  }
+
+  if (data.notes[id] === undefined) {
+    res.status(404);
+    res.json({ error: `cannot find note with id ${id}` });
+    return;
+  }
+
+  data.notes[id].content = req.body.content;
+
+  fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      console.error(err);
+      res.status(500);
+      res.json({ error: 'An unexpected error occurred' });
+    } else {
+      res.status(200);
+      res.json(data.notes[id]);
+    }
+  });
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
